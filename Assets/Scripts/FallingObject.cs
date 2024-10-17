bc3840b6a97e6c5f58fef1a402d171283c8bae31
Yuulis <yuulis2005@gmail.com>
2024-10-17 23:21:06 +0900

    落下オブジェクトの追加

--- Assets/Scripts/FallingObject.cs
+++ Assets/Scripts/FallingObject.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 
 public enum ObjectType
 {
-    capacitor = 1,
-    drone = 2,
-    plate = 3,
-    robot = 4,
+    battery = 1,
+    board_1 = 2,
+    board_2 = 3,
+    capacitor = 4,
+    drone_1 = 5,
+    drone_2 = 6,
+    iron_1 = 7,
+    iron_2 = 8,
+    iron_3 = 9,
+    plate = 10,
+    robot = 11,
+    screw = 12
 }
 
 
@@ -30,7 +38,7 @@ public class FallingObject : MonoBehaviour
     {
         if (collision.gameObject.tag == "Bar" || collision.gameObject.tag == "GameoverLine")
         {
-            Debug.Log("Hit");
+            // Debug.Log("Hit");
         }
     }
 }
